e9c21e90dd23952ad5a640a05c2cff0ed0e111ff
Nick McIntyre <nick@mcintyre.io>
2022-12-06 17:06:56 -0600

    Start work on tests

---
+++ test/test_sketch.py
@@ -0,0 +1,6 @@
+from jupy5 import Sketch
+
+
+def test_constructor():
+    p5 = Sketch(200, 200)
+    assert p5.width == 200 and p5.height == 200
---
+++ test/test_turtle.py
@@ -0,0 +1,6 @@
+from jupy5 import Turtle
+
+
+def test_constructor():
+    t = Turtle()
+    assert t.width == 200 and t.height == 200
